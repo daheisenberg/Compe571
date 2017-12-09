ee6685a4161c19f38e6889ded6ae7b326262bb5a
daheisenberg <letorres@sdsu.edu>
2017-12-09 09:58:07 +0000

    Added second PIR

--- mainFile.py
+++ mainFile.py
@@ -5,27 +5,40 @@ import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(3,GPIO.OUT)
+GPIO.setup(5,GPIO.OUT)
+
 
 GPIO.setup(11,GPIO.IN)
-
-
+GPIO.setup(12,GPIO.IN)
 
 
 while True:
 
 	i= GPIO.input(11)
+	j= GPIO.input(12)
 
 #hello
 	if i == 0:
-		print "no intruders" , i
+		print "no intruders On left" , i
 		GPIO.output(3,0)
-		time.sleep(1)
+		time.sleep(.3)
 	
 	if i == 1:
-		print "intruder" , i
+		print "intruder On left" , i
 		GPIO.output(3,1)
-		os.system("python captureImage.py") 
-		time.sleep(1)
+		#os.system("python captureImage.py") 
+		time.sleep(.3)
+		
+	if j == 0:
+		print "no intruders On right" , j
+		GPIO.output(5,0)
+		time.sleep(.3)
+		
+	if j == 1:
+		print "intruders On right" , j
+		GPIO.output(5,1)
+		#os.system("python captureImage.py")
+		time.sleep(.3)		
 
 
 
