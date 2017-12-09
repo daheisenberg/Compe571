85099381b155973226b59c70ef1da7d17c00339d
Mheisenberg <letorres@sdsu.edu>
2017-12-09 07:58:09 +0000

    First Commit save my ass!

---
+++ captureImage.py
@@ -0,0 +1,44 @@
+import time
+from subprocess import call
+import os, os.path
+import shutil
+from datetime import datetime
+
+
+#The amperage of the webcam is not enough, it restarts, works 1-10 times.
+#delete previous avi
+#bad idea erases everything!
+#call(["find", "." ,"-name" ,"*.avi" ,"-type" ,"f", "-delete"])
+
+#get the number of files to archive all videos
+
+DIR = '/home/pi/Desktop/Project571/sendToDropbox'
+numberOfFiles = str (len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))]))
+print numberOfFiles
+tokenInt = int(numberOfFiles) + 1
+
+print tokenInt
+numberOfFiles = str(tokenInt)				
+
+print numberOfFiles
+
+newFileName = "video2Go" + numberOfFiles + ".avi"
+
+for i in xrange(1,10):
+	#for image only
+	#call(["fswebcam", "-d", "/dev/video0", "-r", "1280x720", "--no-banner", "thefile.jpg"])
+	
+	call(["avconv", "-t", "3",  "-f",  "video4linux2" , "-i", "/dev/video0", newFileName])
+		
+	time.sleep(1)
+
+
+print newFileName
+
+source = '/home/pi/Desktop/Project571/' + newFileName
+destination = '/home/pi/Desktop/Project571/sendToDropbox/' + newFileName
+
+shutil.copy2(source, destination) # complete target filename given
+
+os.remove(source)
+
---
+++ mainFile.py
@@ -0,0 +1,17 @@
+import os, os.path
+
+
+while True:
+
+	nb = input('Choose a number: ')
+	
+	if(nb == 1):
+		os.system("python captureImage.py") 
+
+
+
+
+
+
+
+
