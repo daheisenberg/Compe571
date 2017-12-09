679e564511c3f1f4cd3129222fab18ad0a1a8335
daheisenberg <letorres@sdsu.edu>
2017-12-09 21:08:16 +0000

    working dropbox

--- sendToDropbox.py
+++ sendToDropbox.py
@@ -1,12 +1,24 @@
 import dropbox
 
+import os
+
+
 import sys
 
-f = open('sendToDropbox/video2Go2.avi', 'rb') 
+
+DIR = '/home/pi/Desktop/Project571/sendToDropbox'
+numberOfFiles = str (len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))]))
+print numberOfFiles
+
+sendFile = str('/intruder'+numberOfFiles+'.avi')
+
+fileToOpen = 'sendToDropbox/video2Go' + numberOfFiles + '.avi'
+
+f = open(fileToOpen, 'rb') 
 
 dbx = dropbox.Dropbox ('gFTH8yYgQ8AAAAAAAAAACoJs3XHwDnOJCtqe9UxA3TxOd9Ed0iKGCJceG3a3kTtV')
 
-dbx.files_upload(f.read(), '/intruder.avi', mute = True)
+dbx.files_upload(f.read(), sendFile, mute = True)
 
 f.close()
 
